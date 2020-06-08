3b5efe7eaf423210f75aa9b2aae346005c388fc5
mmkhan <mariatechfuture@gmail.com>
2020-06-08 17:41:00 -0400

    created employees

---
+++ Develop/lib/Employee.js
@@ -0,0 +1,28 @@
+// TODO: Write code to define and export the Employee class
+//### Classes
+//The project must have the these classes: `Employee`, `Manager`, `Engineer`,
+//`Intern`. The tests for these classes in the `tests` directory must all pass.
+
+//The first class is an `Employee` parent class with the following properties and
+//methods:
+class Employee {
+	constructor(name, id, email) {
+		this.name = name;
+		this.id = id;
+		this.email = email;
+	}
+	getId() {
+		return this.id;
+	}
+	getName() {
+		return this.name;
+	}
+	getEmail() {
+		return this.Email;
+	}
+	getRole() {
+		return `Employee`;
+	}
+}
+
+module.exports = Employee;
